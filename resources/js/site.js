fc82a17fa92ed410fcab4ec8b1442664f252a5c3
Ryan Deming <ryan@ryandeming.com>
2024-11-23 23:36:42 -0500

    add transitions to work

--- resources/js/site.js
+++ resources/js/site.js
@@ -62,19 +62,30 @@ document.addEventListener('DOMContentLoaded', function() {
             
 
             // Remove active class from all portfolio images
-            portfolioImages.forEach(image => image.classList.remove('active'));
+            portfolioImages.forEach(image => {
+                image.classList.remove('active')
+                image.classList.remove('fade-in');
+            });
             // Add active class to the corresponding portfolio image
             const portfolioImage = document.querySelector(`.portfolio-image[data-id="${id}"]`);
 
             // Remove active class from all project descriptions
-            projectDescriptions.forEach(description => description.classList.remove('active'));
+            projectDescriptions.forEach(description => {
+                description.classList.remove('active')
+                description.classList.remove('fade-in');
+            });
+
             // Add active class to the corresponding project description
             const projectDescription = document.querySelector(`.project-description[data-id="${id}"]`);
             if(!portfolioImage || !projectDescription) return;
             this.classList.add('active');
             portfolioImage.classList.add('active');
             projectDescription.classList.add('active');
-            
+
+            setTimeout(() => {
+                portfolioImage.classList.add('fade-in');
+                projectDescription.classList.add('fade-in');
+            }, 10);
         });
     });
 });
